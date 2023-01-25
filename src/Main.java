1189ee0ceac6147b1625a5f3d1a181f8fb55de52
Axel Chavarría Montoya <axelc@grupomundial.cr>
2023-01-25 14:28:59 -0600

    PasswordGenerator challenge resolved

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,11 @@ public class Main {
         /*LenguajeHacker lenguajeHacker = new LenguajeHacker();
         System.out.println(lenguajeHacker.solution("Hola 13"));*/
 
-        PartidoTenis partidoTenis = new PartidoTenis();
+        /*PartidoTenis partidoTenis = new PartidoTenis();
         String[] gameState = {"P2", "P2", "P1", "P1", "P2", "P1", "P2", "P2"};
-        partidoTenis.solution(gameState);
+        partidoTenis.solution(gameState);*/
+
+        PasswordGenerator passwordGenerator = new PasswordGenerator();
+        System.out.println(passwordGenerator.solution(15, true, false, true));
     }
 }
---
+++ src/PasswordGenerator.java
@@ -0,0 +1,28 @@
+public class PasswordGenerator {
+
+    private char[] alphabet;
+    private int[] numbers;
+    private char [] symbols;
+    public PasswordGenerator(){
+        alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+        numbers = new int[]{0,1,2,3,4,5,6,7,8,9};
+        symbols = "!@#$%^&*()_+".toCharArray();
+    }
+    public String solution(int tam, boolean mayusculas, boolean numeros, boolean simbolos){
+        var passw = "";
+
+        for(int i = 1; i <= tam; i++){
+            var randomIndex = randomIndex(0, alphabet.length -1);
+            passw +=alphabet[randomIndex];
+        }
+        if(mayusculas) passw = passw.replace(passw.charAt(0), Character.toUpperCase(passw.charAt(0)));
+        if (numeros) passw = passw.replace(passw.charAt(passw.length() - 1), Character.forDigit(randomIndex(0,numbers.length -1),10));
+        if (simbolos) passw = passw.replace(passw.charAt(2), symbols[randomIndex(0,symbols.length -1)]);
+
+        return passw;
+    }
+
+    private int randomIndex(int min, int max){
+        return (int)Math.floor(Math.random() * (max - min + 1)+ min);
+    }
+}
